f26b190fabd1abec0ef86d86229083a7be989bd7
Marc Doerflinger <github@doerf.li>
2020-04-02 22:49:51 +0200

    add date to commands

--- src/main/kotlin/li/doerf/es/bankaccount/controllers/BalanceController.kt
+++ src/main/kotlin/li/doerf/es/bankaccount/controllers/BalanceController.kt
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.messaging.support.MessageBuilder
 import org.springframework.web.bind.annotation.*
+import java.time.Instant
 
 @RestController
 @RequestMapping("/balance")
@@ -22,7 +23,7 @@ class BalanceController(private val producer: BalanceProducer, private val accou
     @PostMapping("/{accountNumber}/credit")
     fun credit(@PathVariable accountNumber: String, @RequestBody request: CreditRequest): ResponseEntity<HttpStatus> {
         logger.debug("received credit request $request")
-        val msg = MessageBuilder.withPayload(AmountAdded(accountNumber, request.amount)).setHeader("type", "add").build()
+        val msg = MessageBuilder.withPayload(AmountAdded(accountNumber, request.amount, Instant.now())).setHeader("type", "add").build()
         producer.balanceStreams.balanceOut().send(msg)
         logger.debug("msg sent with id: ${msg.headers["id"]}")
         return ResponseEntity.ok().build()
@@ -31,7 +32,7 @@ class BalanceController(private val producer: BalanceProducer, private val accou
     @PostMapping("/{accountNumber}/debit")
     fun debit(@PathVariable accountNumber: String, @RequestBody request: DebitRequest): ResponseEntity<HttpStatus> {
         logger.debug("received debit request $request")
-        val msg = MessageBuilder.withPayload(AmountRemoved(accountNumber, request.amount)).setHeader("type", "remove").build()
+        val msg = MessageBuilder.withPayload(AmountRemoved(accountNumber, request.amount, Instant.now())).setHeader("type", "remove").build()
         producer.balanceStreams.balanceOut().send(msg)
         logger.debug("msg sent with id: ${msg.headers["id"]}")
         return ResponseEntity.ok().build()
--- src/main/kotlin/li/doerf/es/bankaccount/streams/Commands.kt
+++ src/main/kotlin/li/doerf/es/bankaccount/streams/Commands.kt
@@ -1,6 +1,7 @@
 package li.doerf.es.bankaccount.streams
 
 import java.math.BigDecimal
+import java.time.Instant
 
-data class AmountAdded(val account: String, val amount: BigDecimal)
-data class AmountRemoved(val account: String, val amount: BigDecimal)
+data class AmountAdded(val account: String, val amount: BigDecimal, val timestamp: Instant)
+data class AmountRemoved(val account: String, val amount: BigDecimal, val timestamp: Instant)
